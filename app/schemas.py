d0962eca168e47ed45cfe95a537582075b17ceff
muhammadahmadazhardatics <muhammad.ahmad@datics.ai>
2025-07-01 11:46:00 +0500

    added optional work

--- app/schemas.py
+++ app/schemas.py
@@ -1,28 +1,31 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List
 from datetime import datetime
 
 class MenuItemCreate(BaseModel):
-    name: str
-    price: float
-    is_available: bool = True
+    name: str = Field(..., example="Chicken Biryani")
+    price: float = Field(..., example=450.0)
+    is_available: bool = Field(default=True, example=True)
 
 class MenuItemOut(BaseModel):
-    id: int
-    name: str
-    price: float
-    is_available: bool
+    id: int = Field(..., example=1)
+    name: str = Field(..., example="Chicken Biryani")
+    price: float = Field(..., example=450.0)
+    is_available: bool = Field(..., example=True)
+
     class Config:
         orm_mode = True
 
 class OrderCreate(BaseModel):
-    customer_name: str
-    item_ids: List[int]
+    customer_name: str = Field(..., example="Ali")
+    item_ids: List[int] = Field(..., example=[1, 2])
 
 class OrderOut(BaseModel):
-    id: int
-    customer_name: str
-    created_at: datetime
+    id: int = Field(..., example=1)
+    customer_name: str = Field(..., example="Ali")
+    created_at: datetime = Field(..., example="2025-07-01T10:30:00")
     items: List[MenuItemOut]
+
     class Config:
         orm_mode = True
+
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+import time
+from fastapi import FastAPI, Request
 from app.routes import menu, order
 
 app = FastAPI()
 
+@app.middleware("http")
+async def log_request_path_and_time(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    duration = time.time() - start_time
+    print(f"[LOG] {request.method} {request.url.path} - {duration:.4f} seconds")
+    return response
+
 app.include_router(menu.router, prefix="/menu", tags=["Menu"])
 app.include_router(order.router, prefix="/order", tags=["Order"])
+
